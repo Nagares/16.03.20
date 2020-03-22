cab11054388c18d1e2fedff33e4500253ff7273e
Anya <youremail@domain.com>
2020-03-22 14:59:03 +0200

    add new branch

---
+++ script.js
@@ -0,0 +1,20 @@
+function addTask(id, task) {
+	const $btnEdit = $('<button>')
+						.addClass('btn btn-warning btn-xs btn-edit pull-right')
+						.html('<i class="glyphicon glyphicon-pencil"></i>')
+
+	const $btnDelete = $('<button>')
+			.addClass('btn btn-danger btn-xs pull-right btn-delete')
+			.html('<i class="glyphicon glyphicon-trash"></i>');
+
+	const $taskItem = $('<li>')
+			.addClass('list-group-item')
+			.text(task.title)
+			.attr('data-id', id)
+			.append($btnDelete)
+			.append($btnEdit);
+
+	$tasksList
+		.find(`[data-status="${task.status}"]`)
+		.append($taskItem);
+}
---
+++ script1.js
@@ -0,0 +1,74 @@
+
+
+const $formAddTask = $('#formAddTask');
+const $tasksList = $('#tasks-list');
+const $modalAddTask = $('#modalAddTask');
+const $formEditTask = $('#formEditTask');
+const $modalEditTask = $('#modalEditTask');
+
+$formAddTask.on('submit', function(event) {
+	event.preventDefault();
+
+	let task = {
+		id: new Date().getTime(),
+		title: $('[name="title"]', this).val(),
+		status: 1 // 1 - todo, 2 - in progress, 3 - done
+	};
+
+	addTask(task.id, task);
+
+	localStorage.setItem(task.id, JSON.stringify(task));
+
+	$modalAddTask.modal('hide');
+
+	this.reset();
+});
+
+for (let key in localStorage) {
+	if (!localStorage.hasOwnProperty(key)) continue;
+
+	const task = JSON.parse(localStorage[key]);
+
+	addTask(task.id, task);
+}
+
+$('body').on('click', '.btn-edit', function() {
+	const $parent = $(this).parents('[data-id]');
+
+	const taskId = $parent.data('id');
+
+	const task = JSON.parse(localStorage.getItem(taskId));
+
+	for (let key in task) {
+		$formEditTask.find(`[name="${key}"]`).val(task[key]);
+	}
+
+	$modalEditTask.modal('show');
+});
+
+$('body').on('click', '.btn-delete', function(event) {
+	const $parent = $(this).parents('[data-id]');
+
+	const taskId = $parent.data('id');
+	$parent.remove();
+
+	localStorage.removeItem(taskId);
+});
+
+$formEditTask.on('submit', function(event) {
+	event.preventDefault();
+
+	const task = {
+		title: $('[name="title"]', this).val(),
+		status: $('[name="status"]', this).val(),
+		id: $('[name="id"]', this).val()
+	};
+
+	$tasksList.find(`[data-id="${task.id}"]`).remove();
+
+	addTask(task.id, task);
+
+	$modalEditTask.modal('hide');
+
+	localStorage.setItem(task.id, JSON.stringify(task));
+});
